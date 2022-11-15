a8045d9c151ccf490a088ce7c3c928bed9de8e24
Jorge Sorondo <87992063+jisorondo@users.noreply.github.com>
2022-11-15 17:17:40 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "git::ssh://github.com/jisorondo/terraform-aws-s3-website.git"
+  source  = "git::ssh://jisorondo@github.com/terraform-aws-s3-website.git"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
